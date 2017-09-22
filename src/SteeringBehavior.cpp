a2b2c791de460da73e967db633651bb1bb28cb83
Binrong Zheng <bzheng@enti.local>
2017-09-22 19:30:13 +0200

    Add project files.

---
+++ src/SteeringBehavior.cpp
@@ -0,0 +1,115 @@
+#include "SteeringBehavior.h"
+
+
+
+SteeringBehavior::SteeringBehavior()
+{
+}
+
+
+SteeringBehavior::~SteeringBehavior()
+{
+}
+
+Vector2D SteeringBehavior::KinematicSeek(Agent *agent, Vector2D target, float dtime)
+{
+	Vector2D steering = target - agent->position;
+	steering.Normalize();
+	return steering * agent->max_velocity;
+}
+
+Vector2D SteeringBehavior::KinematicSeek(Agent *agent, Agent *target, float dtime)
+{
+	return KinematicSeek(agent, target->position, dtime);
+}
+
+Vector2D SteeringBehavior::KinematicFlee(Agent *agent, Vector2D target, float dtime)
+{
+	Vector2D steering = agent->position - target;
+	steering.Normalize();
+	return steering * agent->max_velocity;
+}
+
+Vector2D SteeringBehavior::KinematicFlee(Agent *agent, Agent *target, float dtime)
+{
+	return KinematicFlee(agent, target->position, dtime);
+}
+
+
+/* Add here your own Steering Behavior functions definitions */
+/*
+
+
+Vector2D SteeringBehavior::Seek(Agent *agent, Vector2D target, float dtime)
+{
+	return Vector2D(0, 0);
+
+}
+*/
+
+Vector2D SteeringBehavior::Seek(Agent *agent, Vector2D target, float dtime)
+{
+	Vector2D dv = target - agent->position;
+	dv.Normalize();
+	dv *= agent->max_velocity;
+	Vector2D sf = dv - agent->velocity;
+	sf /= agent->getMaxVelocity();
+
+	return sf * agent->max_force;
+}
+
+Vector2D SteeringBehavior::Seek(Agent *agent, Agent *target, float dtime)
+{
+	return Seek(agent, target->position, dtime);
+}
+
+
+Vector2D SteeringBehavior::Flee(Agent *agent, Vector2D target, float dtime)
+{
+	Vector2D dv = agent->position- target ;
+	dv.Normalize();
+	dv *= agent->max_velocity;
+	Vector2D sf = dv - agent->velocity;
+	sf /= agent->getMaxVelocity();
+
+	return sf * agent->max_force;
+}
+
+Vector2D SteeringBehavior::Flee(Agent *agent, Agent *target, float dtime)
+{
+	return Flee(agent, target->position, dtime);
+}
+
+Vector2D SteeringBehavior::Arrive(Agent *agent, Vector2D target, float radiArea, float dtime)
+{
+
+	Vector2D dv = target- agent->position;
+	float Factor;
+	float distance = sqrt(dv.x*dv.x+dv.y*dv.y);
+	if (distance > radiArea) {
+		dv.Normalize();
+		dv *= agent->max_velocity;
+	}
+	else{
+		
+		dv.Normalize();
+		Factor = distance / radiArea;		
+		dv *= agent->max_velocity*Factor;
+	}
+
+	Vector2D sf = dv - agent->velocity;
+	sf /= agent->getMaxVelocity();
+	
+	return sf * agent->max_force;
+}
+
+Vector2D SteeringBehavior::Arrive(Agent *agent, Agent *target, float dtime,float radiArea)
+{
+	return Arrive(agent, target->position, dtime, radiArea);
+}
+/*
+Vector2D SteeringBehavior::Wander(Agent *agent, float angle, float *wanderAngle, float rawanderMaxChangediArea, float  wanderCircleOffset, float wanderCircleRadius,float dtime)
+{
+	//return Wander(agent, target->position, dtime, radiArea);
+}
+*/
---
+++ src/SteeringBehavior.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Agent.h"
+#include "Vector2D.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Agent;
+
+class SteeringBehavior
+{
+public:
+	SteeringBehavior();
+	~SteeringBehavior();
+	Vector2D KinematicSeek(Agent *agent, Vector2D target, float dtime);
+	Vector2D KinematicSeek(Agent *agent, Agent *target, float dtime);
+	Vector2D KinematicFlee(Agent *agent, Vector2D target, float dtime);
+	Vector2D KinematicFlee(Agent *agent, Agent *target, float dtime);
+
+	/* Add here your own Steering Behavior functions declarations */
+	Vector2D Seek(Agent *agent, Vector2D target, float dtime);
+	Vector2D Seek(Agent *agent, Agent *target, float dtime);
+	Vector2D Flee(Agent *agent, Vector2D target, float dtime);
+	Vector2D Flee(Agent *agent, Agent *target, float dtime);
+	//etc...
+	Vector2D Arrive(Agent *agent, Vector2D target, float dtime, float radiArea);
+	Vector2D Arrive(Agent *agent, Agent *target, float dtime, float radiArea);
+//	Vector2D Wander(Agent *agent, float angle, float *wanderAngle, float rawanderMaxChangediArea, float  wanderCircleOffset, float wanderCircleRadius, float dtime);
+
+};
